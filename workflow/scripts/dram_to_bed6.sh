8f1b4734df885ed1f4a53a08eab8edced510181d
Jorge Langa <6546145+jlanga@users.noreply.github.com>
2024-05-14 18:21:49 +0200

    fix: remove unused code

--- workflow/scripts/dram_to_bed6.sh
+++ workflow/scripts/dram_to_bed6.sh
@@ -7,24 +7,3 @@ tail -n+2 "$@" \
     -v OFS="\t" \
     '{print $3, $5 - 1, $6, $1, 1000, ($7 == 1 ? "+" : "-")}' \
 | bedtools sort
-
-
-
-
-bedtools intersect \
-    -a test.bam \
-    -b annotations.tsv \
-    -wb
-
-
-samtools view -h test.bam | bam2bed - | bedmap --echo --count annotations.bed6
-
-
-bedtools intersect \
-    -a annotations.bed6 \
-    -b test.bam \
-    -c -bed \
-| cut -f 4,7 \
-| sort \
-| uniq --count \
-| awk -v OFS="\t" '{print $2, $1}'
